f5f13861dd304dde166245754931a75ee4744c98
yamagai <kohei.yamamoto919@gmail.com>
2020-05-23 19:29:55 +0900

    helpers/albums_helperのテストコード追加

--- spec/factories/album.rb
+++ spec/factories/album.rb
@@ -2,5 +2,14 @@ FactoryBot.define do
   factory :album do
     name { "test_album" }
     album_hash { SecureRandom.alphanumeric(20) }
+
+    trait :with_favorites do
+      after(:build) do |album|
+        picture = create(:picture, picture_name: Rack::Test::UploadedFile.new(Rails.root.join('spec/factories/test.jpg'), 'image/jpeg')) do |p|
+          p.favoriters = album.users
+        end
+        album.pictures << picture
+      end
+    end
   end
 end
