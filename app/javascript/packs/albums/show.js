559214bf70550c306415eb50dfa1f804426c73c8
yamagai <kohei.yamamoto919@gmail.com>
2020-05-26 17:58:50 +0900

    画像押したら画像とコメント、コメント押したらコメントだけ表示するようにした

--- app/javascript/packs/albums/show.js
+++ app/javascript/packs/albums/show.js
@@ -4,19 +4,24 @@ function popupImage() {
 
   $('.js-show-popup').on("click", function(){
     if($(this).prop('tagName') == 'IMG'){
+      $('.popup-content').prepend('<img>');
       var imgsrc = $(this).attr('src');
-      $('.popup-inner').find('img').attr('src',imgsrc);
+      $('.popup-content').find('img').attr('src',imgsrc);
     }
     popup.classList.toggle('is-show');
   });
 
   $('#js-close-btn').on("click", function(){
-    $('.popup-content').html('<img>');
+    $('.popup-content').find('img').remove();
+    $('.popup-comment').remove();
+    $('.popup-content').append('<div class="popup-comment"></div>');
     popup.classList.toggle('is-show');
   });
 
   $('#js-black-bg').on("click", function(){
-    $('.popup-content').html('<img>');
+    $('.popup-content').find('img').remove();
+    $('.popup-comment').remove();
+    $('.popup-content').append('<div class="popup-comment"></div>');
     popup.classList.toggle('is-show');
   });
 }
