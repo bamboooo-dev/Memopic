5617d21752510992f8a39eb32c3c58e4da06aead
yamagai <kohei.yamamoto919@gmail.com>
2020-05-26 14:22:22 +0900

    コメント投稿機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
   def index
     @picture = Picture.find(params[:picture_id])
     @comments = @picture.comments.order(created_at: :desc)
@@ -7,4 +9,22 @@ class CommentsController < ApplicationController
       format.js
     end
   end
+
+  def create
+    @picture = Picture.find(params[:picture_id])
+    current_user.join(@picture.album) unless current_user.joining?(@picture.album)
+    @comment = @picture.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    respond_to do |format|
+      format.html { redirect_to @picture.album }
+      format.js
+    end
+  end
+
+  private
+
+    def comment_params
+      params.permit(:content, :picture_id)
+    end
 end
