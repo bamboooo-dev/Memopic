88507bd8e018a689434406212c0d39346557d763
Yuya Sumie <monamonacan0926@gmail.com>
2020-06-10 00:06:20 +0900

    コメントのテスト作成

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,5 +1,141 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
+  let(:user) do
+    create(:user)
+  end
+  let(:second_user) do
+    create(:second_user)
+  end
+  let(:picture) do
+    build(:picture, picture_name: Rack::Test::UploadedFile.new(Rails.root.join('spec/factories/test.jpg'), 'image/jpeg'))
+  end
+  let!(:album) do
+    Album.create(name:'test_album', album_hash: SecureRandom.alphanumeric(20), users: [ user ], pictures: [ picture ])
+  end
 
+  describe "GET /index" do
+    it "renders a successful response" do
+      get comments_path(picture_id: picture.id)
+      expect(response).to be_successful
+    end
+  end
+
+  context 'ユーザーがアルバムに参加している場合' do
+
+    before do
+      sign_in user
+    end
+
+    describe "POST /comments" do
+      context "with valid parameters" do
+        it "creates a new comment" do
+          expect {
+            post comments_path, params: {
+              content: "テストコメント",
+              picture_id: picture.id,
+            }
+          }.to change(Comment, :count).by(1)
+        end
+      end
+
+      context "with invalid parameters" do
+        it "does not create a new comment" do
+          expect {
+            post comments_path, params: {
+              content: "",
+              picture_id: picture.id,
+            }
+          }.not_to change(Comment, :count)
+        end
+      end
+    end
+
+    describe "DELETE /comments/:id" do
+
+      before do
+        post comments_path, params: {
+          content: "テストコメント",
+          picture_id: picture.id,
+        }
+      end
+
+      it 'destroys existing comment' do
+        expect {
+          delete comment_path( picture.comments.first )
+        }.to change(Comment, :count).by(-1)
+      end
+
+    end
+  end
+
+  context 'ユーザーがアルバムに参加していない場合' do
+
+    before do
+      sign_in second_user
+    end
+
+    describe "POST /comments" do
+      context "with valid parameters" do
+        it "creates a new comment" do
+          expect {
+            post comments_path, params: {
+              content: "テストコメント",
+              picture_id: picture.id,
+            }
+          }.to change(Comment, :count).by(1)
+        end
+
+        it "creates a user_album relationship" do
+          expect {
+            post comments_path, params: {
+              content: "テストコメント",
+              picture_id: picture.id,
+            }
+          }.to change(UserAlbum, :count).by(1)
+        end
+      end
+
+      context "with invalid parameters" do
+        it "doesn't create a new comment" do
+          expect {
+            post comments_path, params: {
+              content: "",
+              picture_id: picture.id,
+            }
+          }.not_to change(Comment, :count)
+        end
+
+        it "doesn't creates a user_album relationship" do
+          expect {
+            post comments_path, params: {
+              content: "",
+              picture_id: picture.id,
+            }
+          }.not_to change(UserAlbum, :count)
+        end
+      end
+    end
+
+    describe "DELETE /comments/:id" do
+
+      before do
+        sign_out second_user
+        sign_in user
+        post comments_path, params: {
+          content: "テストコメント",
+          picture_id: picture.id,
+        }
+        sign_out user
+        sign_in second_user
+      end
+
+      it 'cannot destroy existing comment' do
+        expect {
+          delete comment_path( picture.comments.first )
+        }.not_to change(Comment, :count)
+      end
+
+    end
+  end
 end
---
+++ spec/system/comments_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+RSpec.describe "Comment page", type: :system do
+  describe "コメントページでの挙動" do
+    before do
+      OmniAuth.config.mock_auth[:google_oauth2] = nil
+      Rails.application.env_config['omniauth.auth'] = set_omniauth :google_oauth2
+      visit root_path
+      click_on 'Google Signin'
+      click_button 'アカウント登録'
+      click_button '新しいアルバムを作成する'
+      sleep 3
+      fill_in 'タイトルを追加', with: 'テストアルバム'
+      file_path = Rails.root.join('spec', 'factories', 'test.jpg')
+      attach_file('album_form_pictures', file_path, make_visible: true)
+      click_button 'アップロードする'
+      sleep 5
+    end
+
+    it 'コメントアイコンでコメント一覧が開ける', js: true do
+      find('a.js-show-popup').click
+      expect(page).to have_content 'コメント'
+    end
+
+    it '画像を押すとコメント一覧が開ける', js: true do
+      find('img.js-show-popup').click
+      expect(page).to have_content 'コメント'
+    end
+
+    it 'コメントアイコンからコメントが投稿できる', js: true do
+      find('a.js-show-popup').click
+      fill_in 'コメントする...', with: 'テストコメント'
+      expect {
+        click_on '投稿する'
+        sleep 3
+      }.to change(Comment, :count).by(1)
+      expect(page).to have_content 'テストコメント'
+      expect(page).to have_content 'mockuser'
+      expect(page).to have_link nil, class: 'comment-delete'
+    end
+
+    it '画像アイコンからコメントが投稿できる', js: true do
+      find('img.js-show-popup').click
+      fill_in 'コメントする...', with: 'テストコメント'
+      expect {
+        click_on '投稿する'
+        sleep 3
+      }.to change(Comment, :count).by(1)
+      expect(page).to have_content 'テストコメント'
+      expect(page).to have_content 'mockuser'
+      expect(page).to have_link nil, class: 'comment-delete'
+    end
+
+    it 'ゴミ箱アイコンからコメントが削除できる', js: true do
+      find('img.js-show-popup').click
+      fill_in 'コメントする...', with: 'テストコメント'
+      click_on '投稿する'
+      sleep 3
+      find('a.comment-delete').click
+      expect{
+        page.accept_confirm '削除してよろしいですか？'
+        expect(page).not_to have_content 'テストコメント'
+      }.to change(Comment, :count).by(-1)
+      expect(page).not_to have_content 'mockuser'
+      expect(page).not_to have_link nil, class: 'comment-delete'
+    end
+
+    it 'コメント削除がキャンセルできる', js: true do
+      find('img.js-show-popup').click
+      fill_in 'コメントする...', with: 'テストコメント'
+      click_on '投稿する'
+      sleep 3
+      find('a.comment-delete').click
+      expect{
+        page.accept_confirm '削除してよろしいですか？'
+        expect(page).to have_content 'テストコメント'
+      }.not_to change(Comment, :count)
+      expect(page).to have_content 'mockuser'
+      expect(page).to have_link nil, class: 'comment-delete'
+    end
+
+  end
+end
