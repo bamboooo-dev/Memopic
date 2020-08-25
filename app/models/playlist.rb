f19015d3dfb5ef04699160e670feb4ad2754529a
yamagai <kohei.yamamoto919@gmail.com>
2020-08-25 17:39:06 +0900

    プレイリスト作成時のvalidation追加

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,13 @@
 class Playlist < ApplicationRecord
   belongs_to :album
+
+  validates :name, presence: true
+  validates :url, presence: true
+  validate :validate_playlist_url
+
+  def validate_playlist_url
+    unless url.include?('https://open.spotify.com') && url.include?('https://music.apple.com')
+      errors.add(:url, "に登録できるのはSpotifyまたはAppleMusicのプレイリストだけです") 
+    end
+  end
 end
