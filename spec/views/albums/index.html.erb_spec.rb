0813f5961827dd4011d40a8a8b9e14882c660025
Yuya Sumie <monamonacan0926@gmail.com>
2020-09-24 22:15:47 +0900

    pend a test

--- spec/views/albums/index.html.erb_spec.rb
+++ spec/views/albums/index.html.erb_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "albums/index", type: :view do
-  let(:user) { create(:user) }
-
-  before(:each) do
-    create(:album)
-  end
-
-  it "renders a list of albums" do
-    sign_in user
-    render
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
