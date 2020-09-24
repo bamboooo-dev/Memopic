f71f5cda0e4f49206388a1aec8d0a17d7a6dacd3
Yuya Sumie <monamonacan0926@gmail.com>
2020-09-24 22:05:58 +0900

    fix for tests

--- spec/views/albums/index.html.erb_spec.rb
+++ spec/views/albums/index.html.erb_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "albums/index", type: :view do
+  let(:user) { create(:user) }
+
   before(:each) do
     create(:album)
   end
 
   it "renders a list of albums" do
+    sign_in user
     render
   end
 end
