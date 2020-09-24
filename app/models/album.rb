acc7b657f6d4f17aebf13530024b5e7b93dce6da
Yuya Sumie <monamonacan0926@gmail.com>
2020-09-25 01:10:07 +0900

    fix bugs

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ApplicationRecord
   has_many :pictures, dependent: :destroy
   has_many :playlists, dependent: :destroy
-  has_many :user_albums
+  has_many :user_albums, dependent: :destroy
   has_many :users, through: :user_albums
   validates :name, presence: true
 
